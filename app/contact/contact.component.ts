a011020c024d2530652c37af4d33464636fbe56d
richard-fagot <richard.fagot@gmail.com>
2016-10-30 16:02:27 +0100

    Mise en page 'Contact'

--- app/contact/contact.component.ts
+++ app/contact/contact.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
   moduleId: module.id,
   selector: 'contact',
   templateUrl: 'contact.component.html',
+  styleUrls: ['contact.component.css']
 })
 
 export class ContactComponent {
