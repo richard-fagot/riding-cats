b462db15cbd91a8f0437b2057e6b4e4096989960
richard-fagot <richard.fagot@gmail.com>
2017-01-15 13:11:37 +0100

    Nouvelles dates de concert

--- app/concerts/concerts.component.ts
+++ app/concerts/concerts.component.ts
@@ -12,10 +12,12 @@ export class ConcertsComponent {
   events = [
   { year: '2017',
     events: [
+      {day:'02', month:'SEP', title:'2ème Festival de Rockabilly', location:'Place de la maire, Tarbes', img:'../../assets/img/events/festival-tarbes.jpg', fbLink:''},
       {day:'04', month:'AOU', title:'Journées Coyote', location:'Nogaro', img:'../../assets/img/events/coyote-nogaro.jpg', fbLink:''},
+      {day:'12', month:'JUL', title:'Enregistrement chez Sun Records !', location:'Memphis', img:'../../assets/img/events/sun-studio.jpg', fbLink:''},
       {day:'02', month:'JUL', title:'Festival Country', location:'Gragnague', img:'../../assets/img/events/country-frog.jpg', fbLink:''},
-      {day:'20', month:'MAI', title:'copper city', location:'Durfort', img:'../../assets/img/events/copper-city.jpg', fbLink:''},
-      {day:'29', month:'AVR', title:'Concert Rockabilly avec "Riding Cats"', location:'Au Barp', img:'../../assets/img/events/BNW.jpg', fbLink:''},
+      {day:'20', month:'MAI', title:'copper city', location:'Durfort', img:'../../assets/img/events/copper-city.jpg', fbLink:'https://www.facebook.com/events/1132704173479948/'},
+      {day:'29', month:'AVR', title:'Soirée Dansante', location:'Au Barp', img:'../../assets/img/events/soiree-dansante.jpg', fbLink:''},
     ]
   },
   { year:'2016',
